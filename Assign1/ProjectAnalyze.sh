e8d8913edcfa84c36a0e208c3419d2575bd509f5
Kuber Khanna <khannk1@mills.cas.mcmaster.ca>
2018-02-26 19:11:56 -0500

    Added ProjectAnalyze to Assign1

---
+++ Assign1/ProjectAnalyze.sh
@@ -0,0 +1,48 @@
+ecks status of local repository with respect to remote repository
+function checkstatus()
+{
+git fetch origin 
+echo $(git status)
+}
+
+#Puts all uncommitted changes to changes.log
+function redirectChanges()
+{
+#git stash pop >> changes.log
+git diff >> changes.log
+}
+#Puts each line from every file of your project with the tag TODO into a file todo.log
+function todo()
+{
+grep -r "#TODO" >> todo.log --exclude "todo.log" --exclude "ProjectAnalyze.sh"
+}
+#Checks all haskell files for syntax errors and puts the results into error.log
+function checkHaskell()
+{
+find -name "*.hs" -exec  ghc -fno-code {} \;>>error.log 2>&1
+
+}
+
+
+function pagebypage()
+{
+find . -type f -name "*.txt" -exec  more {} \;>>page.log
+}
+
+function firstfew()
+{
+find . -type f -name "*.txt" -exec  head -n 5  {} \;>>head.log
+}
+
+function lastfew()
+{
+find . -type f -name "*.txt" -exec  tail -n 5  {} \;>>tail.log
+}
+
+checkstatus
+redirectChanges
+todo
+checkHaskell
+pagebypage
+firstfew
+lastfew
